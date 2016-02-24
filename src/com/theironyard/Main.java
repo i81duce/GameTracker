515f50d3dbbcf8b654226545731310191c6e19f5
KevinBozic <bozic.kevin@gmail.com>
2016-02-24 09:54:54 -0500

    Add CSS styling

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -12,6 +12,7 @@ public class Main {
     static HashMap<String , User> users = new HashMap<>();
 
     public static void main(String[] args) {
+        Spark.externalStaticFileLocation("public");
         Spark.init();
         Spark.get(
                 "/",
